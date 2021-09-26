1f8bddd0eb26b1aa592d7f56b31cbd12f05ab2d8
li'mign'liang <lmingl@yeah.net>
2021-09-26 09:08:52 +0800

    映射修改

--- doublekit-wiki-api/src/main/java/com/doublekit/wiki/document/model/Like.java
+++ doublekit-wiki-api/src/main/java/com/doublekit/wiki/document/model/Like.java
@@ -2,7 +2,6 @@ package com.doublekit.wiki.document.model;
 
 import com.doublekit.apibox.annotation.ApiModel;
 import com.doublekit.apibox.annotation.ApiProperty;
-import com.doublekit.beans.annotation.Mapper;
 import com.doublekit.beans.annotation.Mapping;
 import com.doublekit.beans.annotation.Mappings;
 import com.doublekit.common.BaseModel;
