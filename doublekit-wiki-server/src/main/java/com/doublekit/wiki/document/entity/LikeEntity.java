df27266a11406accf94a04af56709e93bfd807a9
zhangzhihua <zhangzhihua@darthcloud.com>
2021-11-10 09:25:41 +0800

    update

--- doublekit-wiki-server/src/main/java/com/doublekit/wiki/document/entity/LikeEntity.java
+++ doublekit-wiki-server/src/main/java/com/doublekit/wiki/document/entity/LikeEntity.java
@@ -8,7 +8,8 @@ import com.doublekit.dal.jpa.mapper.annotation.Table;import com.doublekit.dal.jp
 import java.io.Serializable;
 import java.util.Date;
 
-@Entity @Table(name="wiki_like")
+@Entity
+@Table(name="wiki_like")
 public class LikeEntity implements Serializable {
 
     @Id
