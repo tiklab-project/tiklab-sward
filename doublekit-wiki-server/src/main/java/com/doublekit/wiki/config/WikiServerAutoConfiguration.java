86d2096198bf85da360d01db55b85e39b5b27ed5
袁婕轩 <yuanjiexuan@darthcloud.com>
2021-11-20 16:19:06 +0800

    统计事项

--- doublekit-wiki-server/src/main/java/com/doublekit/wiki/config/WikiServerAutoConfiguration.java
+++ doublekit-wiki-server/src/main/java/com/doublekit/wiki/config/WikiServerAutoConfiguration.java
@@ -1,16 +1,14 @@
 package com.doublekit.wiki.config;
 
 
-import com.doublekit.datafly.starter.annotation.DataFly;
+import com.doublekit.datafly.annotation.DataFly;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@DataFly(locations={
-        "scripts/wiki.sql"
-})
+@DataFly(modules = "wiki")
 @ComponentScan({"com.doublekit.wiki"})
 public class WikiServerAutoConfiguration {
 
