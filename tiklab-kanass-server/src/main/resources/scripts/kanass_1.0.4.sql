b2f8b1813e8d697146fc684f0c6000e1b604eee0
yuanjiexuan <yuanjiexuan@darthcloud.com>
2023-01-14 18:40:52 +0800

    修改wiki样式

--- tiklab-kanass-server/src/main/resources/scripts/kanass_1.0.4.sql
+++ tiklab-kanass-server/src/main/resources/scripts/kanass_1.0.4.sql
@@ -1,1 +1,1 @@
-INSERT INTO `pcs_prc_role` (`id`, `name`, `description`, `grouper`, `type`, `bgroup`) VALUES ('ced47c5eb75c33771e8ab9d02f898908', 'admin', '管理员', 'system', '1', 'kanass');
+INSERT INTO `pcs_prc_role` (`id`, `name`, `description`, `grouper`, `type`, `scope`, `business_type`) VALUES ('1', 'admin', '管理员', 'system', '1', 1, 1);
