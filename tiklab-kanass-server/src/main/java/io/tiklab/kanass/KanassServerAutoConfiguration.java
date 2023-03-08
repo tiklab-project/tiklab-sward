0f52c21a92427311c56f1b8ff7625dc1da395981
yuanjiexuan <yuanjiexuan@darthcloud.com>
2023-03-08 20:34:49 +0800

    接入平台组件

--- tiklab-kanass-server/src/main/java/io/tiklab/kanass/KanassServerAutoConfiguration.java
+++ tiklab-kanass-server/src/main/java/io/tiklab/kanass/KanassServerAutoConfiguration.java
@@ -1,16 +1,21 @@
 package io.tiklab.kanass;
 
-
-import io.tiklab.dsm.annotation.SQL;
+import io.tiklab.dsm.model.SQL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SQL(modules = "kanass")
 @ComponentScan({"io.tiklab.kanass"})
 public class KanassServerAutoConfiguration {
 
     private static Logger logger = LoggerFactory.getLogger(KanassServerAutoConfiguration.class);
+    @Bean
+    SQL initSQL(){
+        return new SQL(new String[]{
+                "kanass"
+        },101);
+    }
 }
