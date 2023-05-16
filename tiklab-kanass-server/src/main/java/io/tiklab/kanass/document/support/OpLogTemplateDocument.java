cf4dd35c59c2830a0186d60cebc685c715be579b
yuanjiexuan <yuanjiexuan@darthcloud.com>
2023-05-16 13:43:31 +0800

    更新日志组件

--- tiklab-kanass-server/src/main/java/io/tiklab/kanass/document/support/OpLogTemplateDocument.java
+++ tiklab-kanass-server/src/main/java/io/tiklab/kanass/document/support/OpLogTemplateDocument.java
@@ -3,9 +3,9 @@ package io.tiklab.kanass.document.support;
 public class OpLogTemplateDocument {
 
     //目录添加模板
-    public static final String TEAMWIRE_LOGTEMPLATE_DOCUMENTADD = "75cc52e77f4ce9dfc1370c906078874d";
+    public static final String TEAMWIRE_LOGTEMPLATE_DOCUMENTADD = "75cc52e77f4c";
 
     //目录添加类型
-    public static final String TEAMWIRE_LOGTYPE_DOCUMENTADD = "49e1eca2fffd1dbfe5e2cbe58cc38a20";
+    public static final String TEAMWIRE_LOGTYPE_DOCUMENTADD = "49e1eca2fffd";
 
 }
