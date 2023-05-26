27187be709ace78ea03122000beb698e328f8621
yuanjiexuan <yuanjiexuan@darthcloud.com>
2023-05-26 17:37:07 +0800

    集成teamwire

--- tiklab-kanass-server/src/main/resources/scripts/kanass_1.0.5_dml.sql
+++ tiklab-kanass-server/src/main/resources/scripts/kanass_1.0.5_dml.sql
@@ -1,1 +1,1 @@
-INSERT INTO pmc_system_url (id, name, system_url, web_url) VALUES ('f7ee9a10', 'teamwire', 'http://bac.teamwire.tiklab.net', 'http://bac.teamwire.tiklab.net');
+INSERT INTO pmc_system_url (id, name, system_url, web_url) VALUES ('f7ee9a10', 'teamwire', 'http://teamwire.tiklab.net', 'http://teamwire.tiklab.net');
