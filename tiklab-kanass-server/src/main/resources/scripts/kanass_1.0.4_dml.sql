741525b18dbef6ec7f7c81040ccb2ea053ac5da5
袁婕轩 <yuanjiexuan@darthcloud.com>
2023-06-16 16:42:36 +0800

    导入jira

--- tiklab-kanass-server/src/main/resources/scripts/kanass_1.0.4_dml.sql
+++ tiklab-kanass-server/src/main/resources/scripts/kanass_1.0.4_dml.sql
@@ -22,7 +22,6 @@ ALTER TABLE kanass_document_attach ALTER COLUMN document_id SET DATA TYPE varcha
 
 ALTER TABLE kanass_document_template ALTER COLUMN id SET DATA TYPE varchar(8);
 
-alter table kanass_comment rename "user" to user_id;
 ALTER TABLE kanass_comment ALTER COLUMN id  SET DATA TYPE varchar(12);
 ALTER TABLE kanass_comment ALTER COLUMN document_id SET DATA TYPE varchar(12);
 ALTER TABLE kanass_comment ALTER COLUMN parent_comment_id SET DATA TYPE varchar(12);
