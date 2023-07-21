cd8c247347862043d30dba582fb182b7ea89beef
袁婕轩 <yuanjiexuan@darthcloud.com>
2023-07-21 09:24:01 +0800

    调试dss

---
+++ tiklab-kanass-server/src/main/java/io/tiklab/kanass/support/service/DssInitIndex.java
@@ -0,0 +1,41 @@
+package io.tiklab.kanass.support.service;
+
+import io.tiklab.dss.client.DssClient;
+import io.tiklab.dss.client.initdata.executer.IndexInitCallback;
+import io.tiklab.kanass.document.model.WikiDocument;
+import io.tiklab.kanass.document.service.DocumentService;
+import io.tiklab.kanass.repository.model.WikiRepository;
+import io.tiklab.kanass.repository.service.WikiRepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DssInitIndex implements IndexInitCallback {
+    @Autowired
+    DssClient dssClient;
+
+    @Autowired
+    WikiRepositoryService wikiRepositoryService;
+
+    @Autowired
+    DocumentService documentService;
+
+    @Override
+    public void execute() {
+        dssClient.deleteAll(WikiRepository.class);
+        dssClient.deleteAll(WikiDocument.class);
+
+        List<WikiRepository> allRepository = wikiRepositoryService.findAllRepository();
+        for (WikiRepository wikiRepository : allRepository) {
+            dssClient.save(wikiRepository);
+        }
+
+        List<WikiDocument> allDocument = documentService.findAllDocument();
+        for (WikiDocument wikiDocument : allDocument) {
+            dssClient.save(wikiDocument);
+        }
+    }
+}
