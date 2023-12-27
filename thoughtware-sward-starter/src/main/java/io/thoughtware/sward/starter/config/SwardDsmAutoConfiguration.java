7661c004d60b43cd82dba2c741cdbce470c61161
袁婕轩 <yuanjiexuan@darthcloud.com>
2023-12-27 23:31:32 +0800

    设置demo 环境

--- thoughtware-sward-starter/src/main/java/io/thoughtware/sward/starter/config/SwardDsmAutoConfiguration.java
+++ thoughtware-sward-starter/src/main/java/io/thoughtware/sward/starter/config/SwardDsmAutoConfiguration.java
@@ -48,13 +48,33 @@ public class SwardDsmAutoConfiguration {
         });
         dsmConfig.newVersion("1.0.2", new String[]{
                 "message_1.0.2",
-                "oplog_1.0.2"
+                "oplog_1.0.2",
+                "kanass_1.0.2",
         });
         dsmConfig.newVersion("1.0.3", new String[]{
-                "message_1.0.3"
+                "message_1.0.3",
+                "kanass_1.0.3"
+        });
+        dsmConfig.newVersion("1.0.4", new String[]{
+                "kanass_1.0.4"
         });
         dsmConfig.newVersion("1.0.5", new String[]{
                 "kanass_1.0.5",
+        });
+        dsmConfig.newVersion("1.0.6", new String[]{
+                "kanass_1.0.6",
+        });
+        dsmConfig.newVersion("1.0.7", new String[]{
+                "kanass_1.0.7",
+        });
+        dsmConfig.newVersion("1.0.8", new String[]{
+                "kanass_1.0.8",
+        });
+        dsmConfig.newVersion("1.0.9", new String[]{
+                "kanass_1.0.9",
+        });
+        dsmConfig.newVersion("1.0.10", new String[]{
+                "kanass_1.0.10",
         });
         return dsmConfig;
     }
