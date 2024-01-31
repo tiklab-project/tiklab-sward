4f0680dbd073df52201825b8a60dbffe417fcf55
袁婕轩 <yuanjiexuan@darthcloud.com>
2024-01-31 21:23:52 +0800

    添加限制人员流程规则

--- thoughtware-sward-starter/src/main/java/io/thoughtware/sward/starter/SwardAutoConfiguration.java
+++ thoughtware-sward-starter/src/main/java/io/thoughtware/sward/starter/SwardAutoConfiguration.java
@@ -13,7 +13,6 @@ import io.thoughtware.eam.boot.starter.annotation.EnableEamClient;
 import io.thoughtware.eam.boot.starter.annotation.EnableEamServer;
 import io.thoughtware.gateway.boot.starter.annotation.EnableGateway;
 import io.thoughtware.licence.boot.starter.annotation.EnableLicenceServer;
-import io.thoughtware.messsage.boot.starter.annotation.EnableMessageClient;
 import io.thoughtware.messsage.boot.starter.annotation.EnableMessageServer;
 import io.thoughtware.openapi.boot.starter.annotation.EnableOpenApi;
 import io.thoughtware.plugin.starter.EnablePluginServer;
@@ -21,10 +20,7 @@ import io.thoughtware.postgresql.EnablePostgresql;
 import io.thoughtware.postin.client.EnablePostInClient;
 import io.thoughtware.privilege.boot.starter.annotation.EnablePrivilegeServer;
 import io.thoughtware.rpc.boot.starter.annotation.EnableRpc;
-import io.thoughtware.security.boot.stater.annotation.EnableSecurityClient;
 import io.thoughtware.security.boot.stater.annotation.EnableSecurityServer;
-import io.thoughtware.todotask.boot.stater.annotation.EnableTodoTaskClient;
-import io.thoughtware.todotask.boot.stater.annotation.EnableTodoTaskServer;
 import io.thoughtware.toolkit.boot.starter.annotation.EnableToolkit;
 import io.thoughtware.user.boot.starter.annotation.EnableUserClient;
 import io.thoughtware.user.boot.starter.annotation.EnableUserServer;
@@ -55,11 +51,7 @@ import org.springframework.context.annotation.Configuration;
 @EnablePrivilegeServer
 @EnableLicenceServer
 @EnableMessageServer
-@EnableMessageClient
 @EnableSecurityServer
-@EnableSecurityClient
-@EnableTodoTaskServer
-@EnableTodoTaskClient
 //other
 @EnableSwardServer
 @EnablePostInClient
