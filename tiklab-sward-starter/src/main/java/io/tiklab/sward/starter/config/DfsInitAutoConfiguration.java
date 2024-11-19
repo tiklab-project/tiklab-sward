a64eafc203b7797c794242f48b293ade21c2274a
袁婕轩 <yuanjiexuan@darthcloud.com>
2024-11-19 18:09:56 +0800

    修改remade

--- tiklab-sward-starter/src/main/java/io/tiklab/sward/starter/config/DfsInitAutoConfiguration.java
+++ tiklab-sward-starter/src/main/java/io/tiklab/sward/starter/config/DfsInitAutoConfiguration.java
@@ -1,7 +1,7 @@
 package io.tiklab.sward.starter.config;
 
 
-import io.tiklab.dfs.client.initdata.config.model.DfsConfig;
+import io.tiklab.dfs.init.initdata.config.model.DfsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
