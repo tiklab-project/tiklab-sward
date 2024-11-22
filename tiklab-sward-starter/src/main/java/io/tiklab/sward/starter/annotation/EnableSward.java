fb4732a3c80ad764d7c28cbfcd2169b45bf977cd
袁婕轩 <yuanjiexuan@darthcloud.com>
2024-11-22 16:50:50 +0800

    修改dfs

--- tiklab-sward-starter/src/main/java/io/tiklab/sward/starter/annotation/EnableSward.java
+++ tiklab-sward-starter/src/main/java/io/tiklab/sward/starter/annotation/EnableSward.java
@@ -9,5 +9,5 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Import({SwardAutoConfiguration.class})
-public @interface EnableKanass {
+public @interface EnableSward {
 }
